321f8087f85b546ee06a2618dfbd5d3b2738c9f1
sarra elmaher <elmahersarra@gmail.com>
2025-08-15 22:03:03 +0100

    create server

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const http = require("http")
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -33,5 +35,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+const server = http.createServer(app)
 
-module.exports = app;
+server.listen(5000,()=>{console.log("app is running on port 5000")})
+
+
